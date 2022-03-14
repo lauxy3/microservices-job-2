1830ac5bbf5d79a99f1f902257baebe20cba925d
wfmobilesg <wfmobilesg@gmail.com>
2022-03-14 13:53:48 +0800

    updated public item

--- Models/BookingItem.cs
+++ Models/BookingItem.cs
@@ -3,8 +3,8 @@ namespace BookingAPI.Models
     public class BookingItem
     {
         public long Id { get; set; }
-        public string? Date { get; set; }
-        public string? Time { get; set; }
+        public DateOnly Date { get; set; }
+        public TimeOnly Time { get; set; }
         public string? PickupPoint { get; set; }
         public string? Destination { get; set; }
         public string? Current_Location_Latitude { get; set; }
