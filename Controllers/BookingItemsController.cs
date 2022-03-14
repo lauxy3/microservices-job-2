6c4dfc7d80a12fb6de01cb7f4ddb2577b0612724
wfmobilesg <wfmobilesg@gmail.com>
2022-03-14 12:51:55 +0800

    update

--- Controllers/BookingItemsController.cs
+++ Controllers/BookingItemsController.cs
@@ -88,8 +88,8 @@ namespace BookingAPI.Controllers
             return NoContent();
         }
 
-        // POST: api/BookingItems
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        //POST: api/BookingItems
+        //To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
         public async Task<ActionResult<BookingItem>> PostBookingItem(BookingItem bookingItem)
         {
@@ -99,6 +99,33 @@ namespace BookingAPI.Controllers
             //return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
             return CreatedAtAction(nameof(GetBookingItem), new { id = bookingItem.Id }, bookingItem);
         }
+
+//         [HttpPost]
+//         public async Task<ActionResult<BookingItem>> PostBookingItem(url='https://jsonplaceholder.typicode.com/users') {
+//          // Default options are marked with *
+//         const response = await fetch(url, {
+//             method: 'POST', // *GET, POST, PUT, DELETE, etc.
+//             mode: 'cors', // no-cors, *cors, same-origin
+//             cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+//             credentials: 'same-origin', // include, *same-origin, omit
+//             headers: {
+//             'Content-Type': 'application/json'
+//                 // 'Content-Type': 'application/x-www-form-urlencoded',
+//                 },
+//                 redirect: 'follow', // manual, *follow, error
+//                 referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+//                 body: JSON.stringify(data) // body data type must match "Content-Type" header
+//             });
+//             return response.json(); // parses JSON response into native JavaScript objects
+//             }
+
+//             postData('https://jsonplaceholder.typicode.com/users', { answer: 42 })
+//             .then(data => {
+//                 console.log(data); // JSON data parsed by `data.json()` call
+//             });
+
+
+
 
         // DELETE: api/BookingItems/5
         [HttpDelete("{id}")]
